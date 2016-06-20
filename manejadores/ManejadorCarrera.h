d4107208ed24f5ccd759669dbb7fe8d7d0c2cb4b
vperez <vperez@Veronica>
2016-06-20 00:56:11 -0300

    creacion de manejadorCarrera y cambios xa modificar estudiante(caso de uso)

---
+++ manejadores/ManejadorCarrera.h
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ManejadorCarrera.h
+ * Author: vperez
+ *
+ * Created on 19 de junio de 2016, 22:52
+ */
+
+#ifndef MANEJADORCARRERA_H
+#define MANEJADORCARRERA_H
+#include <map>
+#include <list>
+#include <string>
+#include "../conceptos/asignatura.h"
+#include "../conceptos/carrera.h"
+
+class ManejadorCarrera {
+public:
+    ManejadorCarrera();
+    ManejadorCarrera(const ManejadorCarrera& orig);
+    
+    static ManejadorCarrera* getInstancia();
+    carrera* getCarrera(string codigo);
+    asignatura* getAsignatura(string codigo);
+    void insertarAsignatura(asignatura*);
+    
+    
+    virtual ~ManejadorCarrera();
+
+private:
+    map<string, asignatura*> asignaturas;
+    map<string, carrera*> carreras;
+    static ManejadorCarrera* instancia;
+};
+
+#endif /* MANEJADORCARRERA_H */
+
